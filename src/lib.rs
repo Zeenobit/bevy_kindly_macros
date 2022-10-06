f7f22029b3b615a93e32d783799f862ea652025c
Zeenobit <zeenobit.exe@gmail.com>
2022-10-06 00:01:57 -0400

    Rename `defaults` to `default_components`
    More verbose, but also more self-documented.

--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,16 @@ fn parse_bundle(attr: &Attribute) -> TokenStream {
     }
 }
 
-#[proc_macro_derive(EntityKind, attributes(defaults, components))]
+#[proc_macro_derive(EntityKind, attributes(default_components, components))]
 pub fn derive_entity_kind(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
 
     let ident = input.ident;
 
-    let defaults = input
+    let default_components = input
         .attrs
         .iter()
-        .find(|attr| attr.path.is_ident("defaults"))
+        .find(|attr| attr.path.is_ident("default_components"))
         .map(|components_attr: &Attribute| parse_bundle(components_attr))
         .unwrap_or_else(|| TokenStream::from(quote! { () }));
 
@@ -37,7 +37,7 @@ pub fn derive_entity_kind(item: proc_macro::TokenStream) -> proc_macro::TokenStr
 
     proc_macro::TokenStream::from(quote! {
         impl bevy_kindly::EntityKind for #ident {
-            type DefaultBundle = #defaults;
+            type DefaultBundle = #default_components;
 
             type Bundle = #components;
 
